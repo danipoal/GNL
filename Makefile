71a265d383f413b266fc906530293427254b35e0
danipoal <danipoal.0@gmail.com>
2025-03-18 15:45:36 +0100

    Norminette fix, bonus set up  & test bonus 1st implementation

--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@ all:
 f fclean:
 	rm -rf *.o *.h.gch *.a
 re: f all
+
+bonus:
+	cc -D BUFFER_SIZE=10 -c -g get_next_line_bonus.c get_next_line_utils_bonus.c get_next_line_bonus.h
+
--- test/test.h
+++ test/test.h
@@ -1,12 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danalvar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/18 15:15:38 by danalvar          #+#    #+#             */
+/*   Updated: 2025/03/18 15:24:30 by danalvar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef TEST_H
 # define TEST_H
 
 # include "../get_next_line.h"
 # include <fcntl.h>
 # include <string.h>
-
-// Duplicadas
-//unsigned long	ft_strlcpy(char *dst, const char *src, unsigned long size);
-//unsigned long	ft_strlen(const char *s);
 
 #endif
